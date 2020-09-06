4f8a2d3b6b22fda2246310af6a0c5bdfc28391a2
ellkka <mihaelatcacib@gmail.com>
2020-09-06 16:36:16 +0300

    Use inputs and display the oldest student

--- src/StudentInstances.java
+++ src/StudentInstances.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Student{
 
     public void name_of_student(String name){
@@ -15,17 +17,38 @@ public class StudentInstances {
 
     public static void main(String[] args){
 
-        Student student1 = new Student();
-        student1.name_of_student("Mihaela");
-        student1.student_age(24);
-        Student.student_status("married");
-        Student student2 = new Student();
-        student2.name_of_student("Max");
-        student2.student_age(28);
-        Student.student_status("not-married");
-        Student student3 = new Student();
-        student3.name_of_student("John");
-        student3.student_age(25);
-        Student.student_status("not-married");
+//        Student student1 = new Student();
+//        student1.name_of_student("Mihaela");
+//        student1.student_age(24);
+//        Student.student_status("married");
+//        Student student2 = new Student();
+//        student2.name_of_student("Max");
+//        student2.student_age(28);
+//        Student.student_status("not-married");
+//        Student student3 = new Student();
+//        student3.name_of_student("John");
+//        student3.student_age(25);
+//        Student.student_status("not-married");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the total number of students:");
+        int numOfStudents = input.nextInt();
+        System.out.println("Enter a student name:");
+        String student1 = input.next();
+        System.out.println("Enter a student age: ");
+        int age1 = input.nextInt();
+        for(int i = 0; i < numOfStudents - 1; i++){
+            System.out.println("Enter a student name: ");
+            String student = input.next();
+            System.out.println("Enter a student age: ");
+            int age = input.nextInt();
+            if (age > age1){
+                student1 = student;
+                age1 = age;
+            }
+        }
+
+        System.out.println("The oldest student is " + student1 + ". He is " + age1 + " old.");
+
     }
 }
